1e3623d8e977bf211ac1c5b25a9f645ce907434d
ssl-oyamata <m.oyamata@jp.fujitsu.com>
2020-04-07 13:30:34 +0900

    Fixed log setting.

--- pgopttune/utils/logger.py
+++ pgopttune/utils/logger.py
@@ -1,73 +1,40 @@
 def logging_dict(debug=False):
+    logging_config = {
+        "version": 1,
+        "disable_existing_loggers": False,
+        "root": {
+            "level": "DEBUG",
+            "handlers": [
+                "consoleHandler",
+                "logFileHandler"
+            ]
+        },
+        "handlers": {
+            "consoleHandler": {
+                "class": "logging.StreamHandler",
+                "level": "INFO",
+                "formatter": "consoleFormatter",
+                "stream": "ext://sys.stdout"
+            },
+            "logFileHandler": {
+                "class": "logging.FileHandler",
+                "level": "INFO",
+                "formatter": "logFileFormatter",
+                "filename": "./log/tune.log",
+                "mode": "a+",
+                "encoding": "utf-8"
+            }
+        },
+        "formatters": {
+            "consoleFormatter": {
+                "format": "[%(asctime)s] [%(levelname)s] %(message)s"
+            },
+            "logFileFormatter": {
+                "format": "[%(asctime)s] [%(levelname)s] [%(filename)s:%(lineno)s] %(message)s"
+            }
+        }
+    }
     if debug:
-        return {
-            "version": 1,
-            "disable_existing_loggers": False,
-            "root": {
-                "level": "DEBUG",
-                "handlers": [
-                    "consoleHandler",
-                    "logFileHandler"
-                ]
-            },
-            "handlers": {
-                "consoleHandler": {
-                    "class": "logging.StreamHandler",
-                    "level": "DEBUG",
-                    "formatter": "consoleFormatter",
-                    "stream": "ext://sys.stdout"
-                },
-                "logFileHandler": {
-                    "class": "logging.FileHandler",
-                    "level": "DEBUG",
-                    "formatter": "logFileFormatter",
-                    "filename": "./log/tune.log",
-                    "mode": "a+",
-                    "encoding": "utf-8"
-                }
-            },
-            "formatters": {
-                "consoleFormatter": {
-                    "format": "[%(asctime)s] [%(levelname)s] %(message)s"
-                },
-                "logFileFormatter": {
-                    "format": "[%(asctime)s] [%(levelname)s] [%(filename)s:%(lineno)s] %(message)s"
-                }
-            }
-        }
-    else:
-        return {
-            "version": 1,
-            "disable_existing_loggers": False,
-            "root": {
-                "level": "DEBUG",
-                "handlers": [
-                    "consoleHandler",
-                    "logFileHandler"
-                ]
-            },
-            "handlers": {
-                "consoleHandler": {
-                    "class": "logging.StreamHandler",
-                    "level": "INFO",
-                    "formatter": "consoleFormatter",
-                    "stream": "ext://sys.stdout"
-                },
-                "logFileHandler": {
-                    "class": "logging.FileHandler",
-                    "level": "INFO",
-                    "formatter": "logFileFormatter",
-                    "filename": "./log/tune.log",
-                    "mode": "a+",
-                    "encoding": "utf-8"
-                }
-            },
-            "formatters": {
-                "consoleFormatter": {
-                    "format": "[%(asctime)s] [%(levelname)s] %(message)s"
-                },
-                "logFileFormatter": {
-                    "format": "[%(asctime)s] [%(levelname)s] [%(filename)s:%(lineno)s] %(message)s"
-                }
-            }
-        }
+        logging_config["handlers"]["consoleHandler"]["level"] = "DEBUG"
+        logging_config["handlers"]["logFileHandler"]["level"] = "DEBUG"
+    return logging_config
