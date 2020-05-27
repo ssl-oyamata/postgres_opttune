70a824a880003fa6f5a259d4a250673e5919d016
ssl-oyamata <ssl-oyamata@users.noreply.github.com>
2020-05-27 23:04:40 +0900

    Fixed data load method to remote database.

--- pgopttune/workload/star_schema_benchmark.py
+++ pgopttune/workload/star_schema_benchmark.py
@@ -6,6 +6,7 @@ from pgopttune.workload.workload import Workload
 from pgopttune.utils.command import run_command
 from pgopttune.config.postgres_server_config import PostgresServerConfig
 from pgopttune.config.star_schema_benchmark_config import StarSchemaBenchmarkConfig
+from pgopttune.utils.pg_connect import get_pg_dsn, get_pg_connection
 
 logger = logging.getLogger(__name__)
 
@@ -19,8 +20,7 @@ class StarSchemaBenchmark(Workload):
         self._create_table()  # create table
         self._truncate_table()  # truncate table
         self._data_file_generate()  # create data file
-        load_sql_filepath = os.path.join(self.ssb_config.sql_file_path, "load.sql")
-        self.execute_sql_file(load_sql_filepath)  # data_load
+        self._load_data()  # data load
         self.vacuum_database()  # vacuum database
 
     def _create_table(self):
@@ -43,8 +43,8 @@ class StarSchemaBenchmark(Workload):
         for table in self.ssb_config.table_list:
             data_filename = table + ".tbl"
             data_filepath = os.path.join(self.ssb_config.ssb_dbgen_path, data_filename)
-            run_cmd_str = "chown {}:{} {}".format(self.postgres_server_config.os_user,
-                                                  self.postgres_server_config.os_user, data_filepath)
+            run_cmd_str = "chown -h {}:{} {}".format(self.postgres_server_config.os_user,
+                                                     self.postgres_server_config.os_user, data_filepath)
             run_command(run_cmd_str, stdout_devnull=True)
 
     def _create_data_file_symlink(self):
@@ -53,12 +53,30 @@ class StarSchemaBenchmark(Workload):
             data_filename = table + ".tbl"
             data_filepath = os.path.join(self.ssb_config.ssb_dbgen_path, data_filename)
             data_symlink_path = os.path.join(dest_dir, data_filename)
-            if os.path.exists(data_symlink_path):
+            if os.path.islink(data_symlink_path):
                 os.unlink(data_symlink_path)
             os.symlink(data_filepath, data_symlink_path)
             run_cmd_str = "chown {}:{} {}".format(self.postgres_server_config.os_user,
                                                   self.postgres_server_config.os_user, data_symlink_path)
             run_command(run_cmd_str, stdout_devnull=True)
+
+    def _load_data(self):
+        if self.postgres_server_config.host == '127.0.0.1' or self.postgres_server_config.host == 'localhost':
+            load_sql_filepath = os.path.join(self.ssb_config.sql_file_path, "load.sql")
+            self.execute_sql_file(load_sql_filepath)
+        else:
+            for table_name in self.ssb_config.table_list:
+                data_filepath = os.path.join("/tmp", table_name + ".tbl")
+                with get_pg_connection(dsn=get_pg_dsn(pghost=self.postgres_server_config.host,
+                                                      pgport=self.postgres_server_config.port,
+                                                      pguser=self.postgres_server_config.user,
+                                                      pgpassword=self.postgres_server_config.password,
+                                                      pgdatabase=self.postgres_server_config.database
+                                                      )) as conn:
+                    conn.set_session(autocommit=True)
+                    with conn.cursor() as cur:
+                        with open(data_filepath) as f:
+                            cur.copy_from(f, table_name, sep='|')
 
     def run(self):
         transaction_tasks = []
@@ -66,7 +84,7 @@ class StarSchemaBenchmark(Workload):
         logger.debug("Start SSB cache warm up.")
         self._run_transaction(self.ssb_config.workload_sql_filepath_list)
         logger.debug("Finish SSB cache warm up.")
-        for i in range(self.ssb_config.clients):
+        for _ in range(self.ssb_config.clients):
             transaction_tasks.append(self.ssb_config.workload_sql_filepath_list)
         start = time.time()
         with Pool(processes=self.ssb_config.clients) as pool:
